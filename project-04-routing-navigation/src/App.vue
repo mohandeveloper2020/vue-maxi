e87ab76b22fe45835db83c4938e1c1fa425fb4ff
Mohan Madheshwaran <mohanmadheshwaran@Mohans-MacBook-Pro.local>
2018-10-01 16:53:21 +0530

    router navigation

---
+++ project-04-routing-navigation/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+
+    <h1>Let's go somewhere</h1>
+
+    <hr/>
+
+    <!-- router-link with path to , changes the page without refreshing browser -->
+    <router-link to="/">Home</router-link>
+    <router-link to="/users">Users</router-link>
+
+    <hr/>
+
+    <!-- 'router-view' is default component created from vue for showing the content of routed page -->
+    <router-view></router-view>
+
+    <!-- routing to home page by method goHome -->
+    <button @click="goHome">Go to Home</button>
+
+  </div>
+</template>
+
+
+<script>
+	
+	export default {
+		methods: {
+			goHome() {
+				this.$router.push('/'); // pushes to the homepage ('/') with $router
+			}
+		}
+	}
+
+</script>
---
+++ project-04-routing-navigation/src/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+  	
+  	<h3>This is home page</h3>
+
+  </div>
+</template>
